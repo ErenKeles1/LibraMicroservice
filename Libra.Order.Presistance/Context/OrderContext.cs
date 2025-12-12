6d0cfe3b07fa2714e9cfffe1f12dfb94f2323f2d
ErenKeles1 <erenkeles741@gmail.com>
2025-12-13 01:29:22 +0300

    Developed the core Order Microservice using Onion Architecture principles. - Integrated CQRS (Command Query Responsibility Segregation) pattern for Address and Order Detail features. - Added full CRUD functionality for Address API endpoints (Command Handlers). - Created Repository and Unit of Work patterns for data access.

---
+++ Libra.Order.Presistance/Context/OrderContext.cs
@@ -0,0 +1,22 @@
+﻿using Libra.Order.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libra.Order.Presistance.Context
+{
+    public class OrderContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;initial Catalog=LibraMicroserviceOrderDb;integrated Security=true;");
+
+        }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Ordering> Orderings { get; set; }
+    }
+}
---
+++ Services/Order/Presentation/Libra.Order.WebApi/Controllers/OrderDetailsController.cs
@@ -0,0 +1,58 @@
+﻿using Libra.Order.Application.Features.CQRS.Commands.OrderDetailCommands;
+using Libra.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using Libra.Order.Application.Features.CQRS.Queries.OrderDetailQueries;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Libra.Order.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderDetailsController : ControllerBase
+    {
+        private readonly GetOrderDetailQueryHandler _getOrderDetailQueryHandler;
+        private readonly GetOrderDetailByIdQueryHandler _getOrderDetailByIdQueryHandler;
+        private readonly CreateOrderDetailCommandHandler _createOrderDetailCommandHandler;
+        private readonly RemoveOrderDetailCommandHandler _removeOrderDetailCommandHandler;
+        private readonly UpdateOrderDetailCommandHandler _UpdateOrderDetailCommandHandler;
+
+        public OrderDetailsController(GetOrderDetailQueryHandler getOrderDetailQueryHandler, GetOrderDetailByIdQueryHandler getOrderDetailByIdQueryHandler, CreateOrderDetailCommandHandler createOrderDetailCommandHandler, RemoveOrderDetailCommandHandler removeOrderDetailCommandHandler, UpdateOrderDetailCommandHandler updateOrderDetailCommandHandler)
+        {
+            _getOrderDetailQueryHandler = getOrderDetailQueryHandler;
+            _getOrderDetailByIdQueryHandler = getOrderDetailByIdQueryHandler;
+            _createOrderDetailCommandHandler = createOrderDetailCommandHandler;
+            _removeOrderDetailCommandHandler = removeOrderDetailCommandHandler;
+            _UpdateOrderDetailCommandHandler = updateOrderDetailCommandHandler;
+        }
+        [HttpGet]
+        public async Task<IActionResult> OrderDetailList()
+        {
+            var values = await _getOrderDetailQueryHandler.Handle();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrderDetailById(int id)
+        {
+            var value = await _getOrderDetailByIdQueryHandler.Handle(new GetOrderDetailByIdQuery(id));
+            return Ok(value);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateOrderDetail(CreateOrderDetailCommand command)
+        {
+            await _createOrderDetailCommandHandler.Handle(command);
+            return Ok("Sipariş detayı başarıyla eklendi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> RemoveOrderDetail(int id)
+        {
+            await _removeOrderDetailCommandHandler.Handle(new RemoveOrderDetailCommand(id));
+            return Ok("Sipariş detayı başarıyla silindi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateOrderDetail(UpdateOrderDetailCommand command)
+        {
+            await _UpdateOrderDetailCommandHandler.Handle(command);
+            return Ok("Sipariş detayı başarıyla güncellendi");
+        }
+    }
+}
